f0166eaf5b8e75bafc42e5ba5dae873392a73dc8
Noha M <nm1488@fayoum.edu.eg>
2021-01-09 21:10:59 +0200

    Updated tab title && favicon

--- npateams/src/app.js
+++ npateams/src/app.js
@@ -5,7 +5,7 @@ import { createStore, applyMiddleware } from 'redux';
 import promiseMiddleware from 'redux-promise';
 import reducers from './reducers';
 import 'aos/dist/aos.css';
-
+import { Helmet } from "react-helmet";
 import TopNavbar from './components/top-navbar/top-navbar';
 import Footer from './components/footer/footer';
 import Teams from './components/teams/teams';
@@ -17,19 +17,27 @@ const createStoreWithMW = applyMiddleware(promiseMiddleware)(createStore);
 function App() {
   return (
     <Provider store={createStoreWithMW(reducers)}>
+
       <BrowserRouter>
-        <TopNavbar />
-        <Switch>
-          <Route path='/teams/:id' component={Team} />
-          <Route path='/teams' component={Teams} />
-          <Route exact path='/' component={Home} />
-          <Route path="*" render={() => (
-            <h1 className="error">
-              404
-            </h1>
-          )} />
-        </Switch>
-        <Footer />
+
+        <Helmet>
+          <title>NPA TEAMS</title>
+          <meta charSet="utf-8" />
+          <meta name="description" content="React Application" />
+          <link rel="icon" type="image/png" href="favicon.ico" sizes="16x16" />
+          <TopNavbar />
+          <Switch>
+            <Route path='/teams/:id' component={Team} />
+            <Route path='/teams' component={Teams} />
+            <Route exact path='/' component={Home} />
+            <Route path="*" render={() => (
+              <h1 className="error">
+                404
+              </h1>
+            )} />
+          </Switch>
+          <Footer />
+        </Helmet>
 
       </BrowserRouter>
     </Provider>
